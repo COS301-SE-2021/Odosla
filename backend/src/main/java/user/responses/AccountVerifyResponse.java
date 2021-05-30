22c3598857cf9a1f58accd02dfe51d5833d2af1b
kelly <u19085878@tuks.co.za>
2021-05-30 17:18:44 +0200

    Added files

---
+++ backend/src/main/java/user/responses/AccountVerifyResponse.java
@@ -0,0 +1,20 @@
+package user.responses;
+
+public class AccountVerifyResponse {
+    /* Attributes */
+    private String token;
+
+    /* Constructor */
+    public AccountVerifyResponse(String token) {
+        this.token = token;
+    }
+
+    /* Getters and Setters */
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
