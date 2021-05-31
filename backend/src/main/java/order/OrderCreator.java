e3fdbbd76f78c8a649bd1724c1b9f8c6d6c80d16
Seamus-Brennan <u19060468@tuks.co.za>
2021-05-31 19:05:19 +0200

    Added OrderCreator file

---
+++ backend/src/main/java/order/OrderCreator.java
@@ -0,0 +1,21 @@
+package order;
+
+public class OrderCreator {
+    /* Attributes */
+    private Order order;
+    /* Constructor */
+    public OrderCreator(){
+
+    }
+    /* Getters and Setters */
+    public Order getOrder(){
+        return order;
+    }
+    public void addItem(Item item, int qty){
+
+    }
+    /* Functions */
+    public void clearOrder(){
+
+    }
+}
