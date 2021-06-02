3f4811fd34d52da7cbc4c97f5ce4c884ce09d02e
Adam <adam.isenberg4920@gmail.com>
2021-06-02 13:21:57 +0200

    Changed assignOrder request object to take in an order object instead of a UUID

--- backend/src/main/java/shopping/requests/AssignOrderRequest.java
+++ backend/src/main/java/shopping/requests/AssignOrderRequest.java
@@ -1,4 +1,19 @@
 package shopping.requests;
 
+import order.dataclass.Order;
+
 public class AssignOrderRequest {
+    private Order order;
+
+    public AssignOrderRequest(Order order) {
+        this.order = order;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
