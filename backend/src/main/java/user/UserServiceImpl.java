5cb3b7a3864d3a32a2718e4343426b0710207351
Adam <adam.isenberg4920@gmail.com>
2021-06-08 11:44:27 +0200

    Added ShopperRepo

--- backend/src/main/java/user/UserServiceImpl.java
+++ backend/src/main/java/user/UserServiceImpl.java
@@ -1,10 +1,32 @@
 package user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import payment.repos.OrderRepo;
+import shopping.ShoppingService;
+import user.repos.ShopperRepo;
 import user.responses.*;
 import user.requests.*;
 
+@Service("userServiceImpl")
 public class UserServiceImpl implements UserService{
 
+    private final ShopperRepo shopperRepo;
+    private final UserService userService;
 
+    @Autowired
+    public UserServiceImpl(ShopperRepo shopperRepo, UserService userService) {
+        this.shopperRepo = shopperRepo;
+        this.userService = userService;
+    }
 
+    @Override
+    public ScanItemResponse scanItem(ScanItemRequest request) {
+        return null;
+    }
+
+    @Override
+    public CompletePackagingOrderResponse completePackagingOrder(CompletePackagingOrderRequest request) {
+        return null;
+    }
 }
