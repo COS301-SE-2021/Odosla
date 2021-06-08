e33c4e3314f4cdb26fba6161289f8a24fbfac19f
Adam <adam.isenberg4920@gmail.com>
2021-06-09 01:16:18 +0200

    fixed application file, added dependencies and properties

--- backend/src/main/java/cs/superleague/Application.java
+++ backend/src/main/java/cs/superleague/Application.java
@@ -1,13 +1,13 @@
 package cs.superleague;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        //SpringBootApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
