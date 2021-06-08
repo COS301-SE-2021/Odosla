7f3b38c598fc5fbf7021a223f91430c2b451fb17
Zivan Haripershad <u19055863@tuks.co.za>
2021-06-08 17:04:08 +0200

    Created GetItemsRequest and GetItemsResponse files and implemented them.

---
+++ backend/src/main/java/shopping/requests/GetItemsRequest.java
@@ -0,0 +1,21 @@
+package shopping.requests;
+
+import java.util.UUID;
+
+public class GetItemsRequest {
+
+    UUID storeID;
+
+    public GetItemsRequest(UUID storeID)
+    {
+        this.storeID= storeID;
+    }
+
+    public void setStoreID(UUID storeID) {
+        this.storeID = storeID;
+    }
+
+    public UUID getStoreID() {
+        return storeID;
+    }
+}
---
+++ backend/src/main/java/shopping/responses/GetItemsResponse.java
@@ -0,0 +1,44 @@
+package shopping.responses;
+
+import shopping.dataclass.Item;
+
+import java.util.Date;
+import java.util.List;
+
+public class GetItemsResponse {
+
+    List<Item> items;
+    Date timestamp;
+    String message;
+
+    GetItemsResponse(List<Item> items, Date timestamp, String message)
+    {
+        this.items = items;
+        this.timestamp= timestamp;
+        this.message= message;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
