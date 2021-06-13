c4950345534d9e71dae9e5629d4ff2cc94c3f139
Adam <adam.isenberg4920@gmail.com>
2021-06-13 14:00:38 +0200

    Swagger code gen error

---
+++ backend/src/main/java/cs/superleague/integration/APIObjectMapper.java
@@ -0,0 +1,12 @@
+package cs.superleague.integration;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class APIObjectMapper {
+
+    //
+
+}
+
--- backend/src/main/java/cs/superleague/shopping/controller/ShoppingController.java
+++ backend/src/main/java/cs/superleague/shopping/controller/ShoppingController.java
@@ -1,13 +1,16 @@
 package cs.superleague.shopping.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
-public class ShoppingController {
+public class ShoppingController { //implements shopping api
 
     @RequestMapping("/test")
     public String test(){
         return "Test complete";
     }
+
 }
