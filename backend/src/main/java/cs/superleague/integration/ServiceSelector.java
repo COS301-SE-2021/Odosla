1cad9e95e955c79e16b541507722550c9f0db093
kelly <u19085878@tuks.co.za>
2021-06-17 18:33:51 +0200

    Updated ServiceSelector

--- backend/src/main/java/cs/superleague/integration/ServiceSelector.java
+++ backend/src/main/java/cs/superleague/integration/ServiceSelector.java
@@ -1,4 +1,45 @@
 package cs.superleague.integration;
 
+import cs.superleague.payment.PaymentService;
+import cs.superleague.shopping.ShoppingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Repository;
+
+@Repository("serviceSelector")
 public class ServiceSelector {
+
+    private static ServiceSelector singleton;
+
+    @Value("${paymentService}")
+    private String paymentService = "";
+
+    @Value("${shoppingService}")
+    private String shoppingService = "";
+
+    private PaymentService payment;
+    private ShoppingService shopping;
+
+    @Autowired
+    public ServiceSelector() {
+        singleton = this;
+    }
+
+    @Autowired
+    public void setServiceSelector(ApplicationContext context) {
+        singleton = this;
+        singleton.payment = (PaymentService) context.getBean(paymentService);
+        singleton.shopping=(ShoppingService) context.getBean(shoppingService);
+
+    }
+
+    public static PaymentService getPaymentService() {
+        return singleton.payment;
+    }
+
+    public static ShoppingService getShoppingService(){
+        return singleton.shopping;
+    }
+
 }
