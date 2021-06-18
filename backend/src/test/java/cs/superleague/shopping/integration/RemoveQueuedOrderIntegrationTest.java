b8eda5dd7ecb5a1918c643631b4349ab074a6f1c
Seamus-Brennan <u19060468@tuks.co.za>
2021-06-18 15:00:41 +0200

    Removed unnecessary lines

--- backend/src/test/java/cs/superleague/shopping/integration/RemoveQueuedOrderIntegrationTest.java
+++ backend/src/test/java/cs/superleague/shopping/integration/RemoveQueuedOrderIntegrationTest.java
@@ -41,8 +41,6 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class RemoveQueuedOrderIntegrationTest {
-    @Autowired
-    ShoppingServiceImpl shoppingService;
 
     @Autowired
     OrderRepo orderRepo;
