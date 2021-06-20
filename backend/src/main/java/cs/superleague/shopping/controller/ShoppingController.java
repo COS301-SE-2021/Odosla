0ac8ed33951615a513884cc73dfeac7a5f8965e4
Adam <adam.isenberg4920@gmail.com>
2021-06-20 02:28:02 +0200

    -added item list to response body -fixed responseEntity in controller function

--- backend/src/main/java/cs/superleague/shopping/controller/ShoppingController.java
+++ backend/src/main/java/cs/superleague/shopping/controller/ShoppingController.java
@@ -3,14 +3,18 @@ package cs.superleague.shopping.controller;
 import cs.superleague.api.LibraryApi;
 import cs.superleague.api.ShoppingApi;
 import cs.superleague.models.Book;
+import cs.superleague.models.ItemObject;
 import cs.superleague.models.ShoppingGetItemsRequest;
 import cs.superleague.models.ShoppingGetItemsResponse;
+import cs.superleague.shopping.dataclass.Item;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,12 +28,16 @@ public class ShoppingController implements ShoppingApi{
     public ResponseEntity<ShoppingGetItemsResponse> getItems(ShoppingGetItemsRequest body) {
 
         //creating response object and default return status:
-        ShoppingGetItemsResponse response = null;
+        ShoppingGetItemsResponse response = new ShoppingGetItemsResponse();
         HttpStatus httpStatus = HttpStatus.OK;
 
         if (mockMode){
-            //response.setJwtToken("298hrf29rh2hr923hgrf2873gt2e4ht");
-            response.setStoreID("ChooseN'Pay_22b");
+            List<ItemObject> mockItems = new ArrayList<>();
+            ItemObject a = new ItemObject();
+            a.setName("mockA");
+            mockItems.add(a);
+
+            response.setItems(mockItems);
         }
 
         return new ResponseEntity<>(response, httpStatus);
