c9d95bc7dcf7d5d21b1ce9d76e0c473ff331acac
kelly <u19085878@tuks.co.za>
2021-06-20 18:24:55 +0200

    Remove unit test and integration test

--- backend/src/test/java/cs/superleague/shopping/integration/RemoveShopperIntegrationTest.java
+++ backend/src/test/java/cs/superleague/shopping/integration/RemoveShopperIntegrationTest.java
@@ -1,24 +1,25 @@
-package cs.superleague.shopping;
+package cs.superleague.shopping.integration;
 
+import cs.superleague.integration.ServiceSelector;
+import cs.superleague.shopping.ShoppingServiceImpl;
+import cs.superleague.shopping.dataclass.Store;
 import cs.superleague.shopping.exceptions.InvalidRequestException;
-import cs.superleague.user.UserService;
+import cs.superleague.shopping.exceptions.StoreDoesNotExistException;
+import cs.superleague.shopping.repos.CatalogueRepo;
+import cs.superleague.shopping.repos.ItemRepo;
+import cs.superleague.shopping.repos.StoreRepo;
+import cs.superleague.shopping.requests.RemoveShopperRequest;
+import cs.superleague.shopping.responses.RemoveShopperResponse;
+import cs.superleague.user.UserServiceImpl;
+import cs.superleague.user.dataclass.Shopper;
 import cs.superleague.user.exceptions.UserDoesNotExistException;
 import cs.superleague.user.repos.ShopperRepo;
 import cs.superleague.user.responses.GetShopperByUUIDResponse;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Description;
-import cs.superleague.shopping.ShoppingServiceImpl;
-import cs.superleague.shopping.dataclass.*;
-import cs.superleague.shopping.exceptions.*;
-import cs.superleague.shopping.requests.*;
-import cs.superleague.shopping.responses.*;
-import cs.superleague.shopping.repos.StoreRepo;
-import cs.superleague.user.dataclass.Shopper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,19 +29,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
-public class RemoveShopperUnitTest {
-    @Mock
-    private StoreRepo storeRepo;
+@SpringBootTest
+public class RemoveShopperIntegrationTest {
 
-    @Mock
-    private ShopperRepo shopperRepo;
+    @Autowired
+    ShoppingServiceImpl shoppingService;
 
-    @Mock(name = "UserServiceImpl")
-    private UserService userService;
+    //OPTIONAL SERVICES
+    @Autowired
+    UserServiceImpl userService;
 
-    @InjectMocks
-    private ShoppingServiceImpl shoppingService;
+    @Autowired
+    StoreRepo storeRepo;
+
+    @Autowired
+    ShopperRepo shopperRepo;
 
     UUID storeUUID1= UUID.randomUUID();
     Store store;
@@ -64,41 +67,47 @@ public class RemoveShopperUnitTest {
         shopper2.setId(shopperID3);
         shopperList.add(shopper1);
         shopperList.add(shopper2);
+        shopperRepo.save(shopper1);
+        shopperRepo.save(shopper2);
     }
 
     @AfterEach
     void tearDown() {
+
+        storeRepo.deleteAll();
+        shopperRepo.deleteAll();
     }
 
     @Test
     @Description("Tests for when removeShoppers is submited with a null request object- exception should be thrown")
     @DisplayName("When request object is not specificed")
-    void UnitTest_testingNullRequestObject(){
-        Throwable thrown = Assertions.assertThrows(InvalidRequestException.class, ()-> shoppingService.removeShopper(null));
+    void IntegrationTest_testingNullRequestObject(){
+        Throwable thrown = Assertions.assertThrows(InvalidRequestException.class, ()-> ServiceSelector.getShoppingService().removeShopper(null));
         assertEquals("Request object can't be null for removeShopper", thrown.getMessage());
     }
 
     @Test
     @Description("Tests for when removeShoppers is submited store ID in request object being null- exception should be thrown")
     @DisplayName("When request object has null parameter")
-    void UnitTest_StoreID_inRequest_NullRequestObject(){
+    void IntegrationTest_StoreID_inRequest_NullRequestObject(){
         RemoveShopperRequest request=new RemoveShopperRequest(shopperID,null);
-        Throwable thrown = Assertions.assertThrows(InvalidRequestException.class, ()-> shoppingService.removeShopper(request));
+        Throwable thrown = Assertions.assertThrows(InvalidRequestException.class, ()-> ServiceSelector.getShoppingService().removeShopper(request));
         assertEquals("Store ID in request object for remove shopper is null", thrown.getMessage());
     }
+
     @Test
     @Description("Tests for when removeShoppers is submited shopper ID in request object being null- exception should be thrown")
     @DisplayName("When request object has null parameter")
-    void UnitTest_ShopperID_inRequest_NullRequestObject(){
+    void IntegrationTest_ShopperID_inRequest_NullRequestObject(){
         RemoveShopperRequest request=new RemoveShopperRequest(null,storeID);
-        Throwable thrown = Assertions.assertThrows(InvalidRequestException.class, ()-> shoppingService.removeShopper(request));
+        Throwable thrown = Assertions.assertThrows(InvalidRequestException.class, ()-> ServiceSelector.getShoppingService().removeShopper(request));
         assertEquals("Shopper ID in request object for remove shopper is null", thrown.getMessage());
     }
 
     @Test
     @Description("Tests whether the removeShoppers request object was created correctly")
     @DisplayName("RemoveShopper Request correctly constructed")
-    void UnitTest_AddShoppersRequestConstruction() {
+    void IntegrationTest_AddShoppersRequestConstruction() {
         RemoveShopperRequest request=new RemoveShopperRequest(shopperID,storeID);
         assertNotNull(request);
         assertEquals(storeID,request.getStoreID());
@@ -108,51 +117,36 @@ public class RemoveShopperUnitTest {
     @Test
     @Description("Test for when Store with storeID does not exist in database - StoreDoesNotExist Exception should be thrown")
     @DisplayName("When Store with ID doesn't exist")
-    void UnitTest_Store_doesnt_exist(){
+    void IntegrationTest_Store_doesnt_exist(){
         RemoveShopperRequest request=new RemoveShopperRequest(shopperID,storeID);
-        when(storeRepo.findById(Mockito.any())).thenReturn(null);
-        Throwable thrown = Assertions.assertThrows(StoreDoesNotExistException.class, ()-> shoppingService.removeShopper(request));
+        Throwable thrown = Assertions.assertThrows(StoreDoesNotExistException.class, ()-> ServiceSelector.getShoppingService().removeShopper(request));
         assertEquals("Store with ID does not exist in repository - could not remove Shopper", thrown.getMessage());
     }
 
-    @Test
-    @Description("Test for when store is return with list of shoppers being null")
-    @DisplayName("List of Shoppers in Store entity is null")
-    void UnitTest_listOfShoppers_isNull() throws InvalidRequestException, cs.superleague.user.exceptions.InvalidRequestException, UserDoesNotExistException, StoreDoesNotExistException {
-        store.setStoreID(storeUUID1);
-        store.setShoppers(null);
-        RemoveShopperRequest request=new RemoveShopperRequest(shopperID,storeID);
-        when(storeRepo.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(store));
-        RemoveShopperResponse response=shoppingService.removeShopper(request);
-        assertEquals(false,response.isSuccess());
-        assertEquals("list of Shoppers is null",response.getMessage());
-    }
 
     @Test
     @Description("Test for when Shopper with shopperID does not exist in shopper database ")
     @DisplayName("When Shopper with ID doesn't exist")
-    void UnitTest_Shopper_doesnt_exist() throws InvalidRequestException, cs.superleague.user.exceptions.InvalidRequestException, UserDoesNotExistException, StoreDoesNotExistException {
+    void IntegrationTest_Shopper_doesnt_exist() throws InvalidRequestException, cs.superleague.user.exceptions.InvalidRequestException, UserDoesNotExistException, StoreDoesNotExistException {
         store.setStoreID(storeUUID1);
         store.setShoppers(shopperList);
         RemoveShopperRequest request=new RemoveShopperRequest(shopperID,storeUUID1);
-        when(storeRepo.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(store));
-        when(userService.getShopperByUUIDRequest(Mockito.any())).thenThrow(new cs.superleague.user.exceptions.InvalidRequestException("User does not exist in database"));
-        Throwable thrown = Assertions.assertThrows(cs.superleague.user.exceptions.InvalidRequestException.class, ()-> shoppingService.removeShopper(request));
-        assertEquals("User does not exist in database", thrown.getMessage());
+        storeRepo.save(store);
+        Throwable thrown = Assertions.assertThrows(UserDoesNotExistException.class, ()-> ServiceSelector.getShoppingService().removeShopper(request));
+        assertEquals("User with ID does not exist in repository - could not get Shopper entity", thrown.getMessage());
     }
 
 
     @Test
     @Description("Test for when list of shoppers doesn't contain shopper")
     @DisplayName("Shopper Id not in list of Shoppers")
-    void UnitTest_shopper_not_in_shopper_list() throws InvalidRequestException, StoreDoesNotExistException, cs.superleague.user.exceptions.InvalidRequestException, UserDoesNotExistException {
+    void IntegrationTest_shopper_not_in_shopper_list() throws InvalidRequestException, StoreDoesNotExistException, cs.superleague.user.exceptions.InvalidRequestException, UserDoesNotExistException {
         store.setStoreID(storeUUID1);
         store.setShoppers(shopperList);
         RemoveShopperRequest request=new RemoveShopperRequest(shopperID,storeUUID1);
-        when(storeRepo.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(store));
-        GetShopperByUUIDResponse shopperResponse=new GetShopperByUUIDResponse(shopper,null,null);
-        when(userService.getShopperByUUIDRequest(Mockito.any())).thenReturn(shopperResponse);
-        RemoveShopperResponse response=shoppingService.removeShopper(request);
+        shopperRepo.save(shopper);
+        storeRepo.save(store);
+        RemoveShopperResponse response=ServiceSelector.getShoppingService().removeShopper(request);
         assertNotNull(response);
         assertEquals(false,response.isSuccess());
         assertEquals("Shopper isn't in list of shoppers in store entity",response.getMessage());
@@ -165,12 +159,18 @@ public class RemoveShopperUnitTest {
         store.setStoreID(storeUUID1);
         store.setShoppers(shopperList);
         RemoveShopperRequest request=new RemoveShopperRequest(shopper1.getId(),storeUUID1);
-        when(storeRepo.findById(Mockito.any())).thenReturn(java.util.Optional.ofNullable(store));
+        storeRepo.save(store);
         GetShopperByUUIDResponse shopperResponse=new GetShopperByUUIDResponse(shopper1,null,null);
-        when(userService.getShopperByUUIDRequest(Mockito.any())).thenReturn(shopperResponse);
-        RemoveShopperResponse response=shoppingService.removeShopper(request);
+        RemoveShopperResponse response=ServiceSelector.getShoppingService().removeShopper(request);
         assertNotNull(response);
         assertEquals(true,response.isSuccess());
         assertEquals("Shopper was successfully removed",response.getMessage());
+        Store storeResponse=storeRepo.findById(store.getStoreID()).orElse(null);
+        assertEquals(shopperList.size()-1,storeResponse.getShoppers().size());
+        List<Shopper> listOfShoppersResponse=storeResponse.getShoppers();
+        shopperList.remove(shopper1);
+        for(int i=0;i<shopperList.size();i++){
+            assertEquals(shopperList.get(i).getId(),listOfShoppersResponse.get(i).getId());
+        }
     }
 }
