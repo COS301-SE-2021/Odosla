1242f21a6d1eabf3ee4153771f9791b0505e1df0
mogoatlhe <u14254922@tuks.co.za>
2021-07-28 11:05:58 +0200

    get invoice request object

--- backend/src/main/java/cs/superleague/payment/requests/GetInvoiceRequest.java
+++ backend/src/main/java/cs/superleague/payment/requests/GetInvoiceRequest.java
@@ -1,4 +1,18 @@
 package cs.superleague.payment.requests;
 
+
+import java.util.UUID;
+
 public class GetInvoiceRequest {
+
+    /** ATTRIBUTES */
+    private final UUID invoiceID;
+
+    public GetInvoiceRequest(UUID invoiceID){
+        this.invoiceID = invoiceID;
+    }
+
+    public UUID getInvoiceID() {
+        return invoiceID;
+    }
 }
