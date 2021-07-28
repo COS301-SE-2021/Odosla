3fe0f79e0013f485bc6268579fab24898755a74a
mogoatlhe <u14254922@tuks.co.za>
2021-07-28 11:13:00 +0200

    get invoice response object

--- backend/src/main/java/cs/superleague/payment/responses/GetInvoiceResponse.java
+++ backend/src/main/java/cs/superleague/payment/responses/GetInvoiceResponse.java
@@ -1,4 +1,36 @@
 package cs.superleague.payment.responses;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class GetInvoiceResponse {
+
+    private final UUID invoiceID;
+    private final byte[] PDF;
+    private final Date timestamp;
+    private final String message;
+
+
+    public GetInvoiceResponse(UUID invoiceID, byte[] PDF, Date timestamp, String message) {
+        this.invoiceID = invoiceID;
+        this.PDF = PDF;
+        this.timestamp = timestamp;
+        this.message = message;
+    }
+
+    public UUID getInvoiceID() {
+        return invoiceID;
+    }
+
+    public byte[] getPDF() {
+        return PDF;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
