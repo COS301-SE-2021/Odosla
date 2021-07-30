9368b426863e2336883b67e6ead9fd6d5678fab3
Seamus-Brennan <u19060468@tuks.co.za>
2021-07-30 17:09:40 +0200

    Added request and response fields for CreateTransaction

--- backend/src/main/java/cs/superleague/payment/requests/CreateTransactionRequest.java
+++ backend/src/main/java/cs/superleague/payment/requests/CreateTransactionRequest.java
@@ -1,4 +1,31 @@
 package cs.superleague.payment.requests;
 
+import java.util.UUID;
+
 public class CreateTransactionRequest {
+    private UUID orderID;
+    private String transactionAddress;
+    //private PayOption card;
+
+
+    public CreateTransactionRequest(UUID orderID, String transactionAddress) {
+        this.orderID = orderID;
+        this.transactionAddress = transactionAddress;
+    }
+
+    public UUID getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(UUID orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getTransactionAddress() {
+        return transactionAddress;
+    }
+
+    public void setTransactionAddress(String transactionAddress) {
+        this.transactionAddress = transactionAddress;
+    }
 }
--- backend/src/main/java/cs/superleague/payment/responses/CreateTransactionResponse.java
+++ backend/src/main/java/cs/superleague/payment/responses/CreateTransactionResponse.java
@@ -1,4 +1,30 @@
 package cs.superleague.payment.responses;
 
+import java.util.Calendar;
+import java.util.UUID;
+
 public class CreateTransactionResponse {
+    private UUID transactionID;
+    private Calendar timestamp;
+
+    public CreateTransactionResponse(UUID transactionID, Calendar timestamp) {
+        this.transactionID = transactionID;
+        this.timestamp = timestamp;
+    }
+
+    public UUID getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(UUID transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public Calendar getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Calendar timestamp) {
+        this.timestamp = timestamp;
+    }
 }
