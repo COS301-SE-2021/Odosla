ca72d42e503d14f3fcc9b086b9788b7916ea8625
mogoatlhe <u14254922@tuks.co.za>
2021-08-01 00:25:10 +0200

    added the customer repo and dat

---
+++ backend/src/main/java/cs/superleague/user/repos/CustomerRepo.java
@@ -0,0 +1,13 @@
+package cs.superleague.user.repos;
+
+import cs.superleague.user.dataclass.Shopper;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Shopper, UUID> {
+
+}
+
