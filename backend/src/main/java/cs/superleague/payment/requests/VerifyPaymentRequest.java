7782a93ad330ab9565c1cd0eca74a32717835d8f
Seamus-Brennan <u19060468@tuks.co.za>
2021-08-01 19:13:40 +0200

    Created request and response objects for VerifyPayment function in PaymentServiceImpl

--- backend/src/main/java/cs/superleague/payment/requests/VerifyPaymentRequest.java
+++ backend/src/main/java/cs/superleague/payment/requests/VerifyPaymentRequest.java
@@ -1,4 +1,19 @@
 package cs.superleague.payment.requests;
 
+import java.util.UUID;
+
 public class VerifyPaymentRequest {
+    private UUID transactionID;
+
+    public VerifyPaymentRequest(UUID transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public UUID getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(UUID transactionID) {
+        this.transactionID = transactionID;
+    }
 }
--- backend/src/main/java/cs/superleague/payment/responses/VerifyPaymentResponse.java
+++ backend/src/main/java/cs/superleague/payment/responses/VerifyPaymentResponse.java
@@ -1,4 +1,30 @@
 package cs.superleague.payment.responses;
 
+import java.util.Calendar;
+import java.util.UUID;
+
 public class VerifyPaymentResponse {
+    private Calendar timestamp;
+    private UUID transactionID;
+
+    public VerifyPaymentResponse(Calendar timestamp, UUID transactionID) {
+        this.timestamp = timestamp;
+        this.transactionID = transactionID;
+    }
+
+    public Calendar getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Calendar timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public UUID getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(UUID transactionID) {
+        this.transactionID = transactionID;
+    }
 }
