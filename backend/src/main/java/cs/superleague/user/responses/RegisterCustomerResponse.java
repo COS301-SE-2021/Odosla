bb3081cc100b719d05694dff3a49dd3c381f6595
mogoatlhe <u14254922@tuks.co.za>
2021-08-02 14:29:54 +0200

    registerCustomer response object

--- backend/src/main/java/cs/superleague/user/responses/RegisterCustomerResponse.java
+++ backend/src/main/java/cs/superleague/user/responses/RegisterCustomerResponse.java
@@ -1,2 +1,27 @@
-package cs.superleague.user.responses;public class RegisterCustomerResponse {
+package cs.superleague.user.responses;
+
+import cs.superleague.user.dataclass.Customer;
+
+public class RegisterCustomerResponse {
+    private final Customer customer;
+    private final boolean success;
+    private final String message;
+
+    public RegisterCustomerResponse(Customer customer, boolean success, String message) {
+        this.customer = customer;
+        this.success = success;
+        this.message = message;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
