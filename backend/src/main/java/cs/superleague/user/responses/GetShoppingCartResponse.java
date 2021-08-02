cc7380f50396145c91c2b2e85f938d3873cad377
mogoatlhe <u14254922@tuks.co.za>
2021-08-02 03:10:28 +0200

    getShoppingCart response object

--- backend/src/main/java/cs/superleague/user/responses/GetShoppingCartResponse.java
+++ backend/src/main/java/cs/superleague/user/responses/GetShoppingCartResponse.java
@@ -1,2 +1,30 @@
-package cs.superleague.user.responses;public class GetShoppingCartResponse {
+package cs.superleague.user.responses;
+
+import cs.superleague.shopping.dataclass.Item;
+
+import java.util.List;
+
+public class GetShoppingCartResponse {
+
+    private final List<Item> shoppingCart;
+    private final String message;
+    private final boolean success;
+
+    public GetShoppingCartResponse(List<Item> shoppingCart, String message, boolean success) {
+        this.shoppingCart = shoppingCart;
+        this.message = message;
+        this.success = success;
+    }
+
+    public List<Item> getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
 }
