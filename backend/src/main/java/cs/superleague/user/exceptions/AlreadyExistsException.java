b76656f3818e2173e72088b3489c5207c0903287
mogoatlhe <u14254922@tuks.co.za>
2021-08-02 14:30:38 +0200

    added the 'AlreadyExistsException'

--- backend/src/main/java/cs/superleague/user/exceptions/AlreadyExistsException.java
+++ backend/src/main/java/cs/superleague/user/exceptions/AlreadyExistsException.java
@@ -1,7 +1,7 @@
 package cs.superleague.user.exceptions;
 
-public class InvalidRequestException extends UserException {
-    public InvalidRequestException(String message){
+public class AlreadyExistsException extends UserException {
+    public AlreadyExistsException(String message){
         super(message);
     }
 }
