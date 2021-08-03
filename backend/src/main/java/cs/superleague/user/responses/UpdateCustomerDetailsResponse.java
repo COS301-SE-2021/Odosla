8384fe19cf4bb2ea9d6200084ef73d8185486892
mogoatlhe <u14254922@tuks.co.za>
2021-08-03 18:29:52 +0200

    updateCustomerDetails response object

--- backend/src/main/java/cs/superleague/user/responses/UpdateCustomerDetailsResponse.java
+++ backend/src/main/java/cs/superleague/user/responses/UpdateCustomerDetailsResponse.java
@@ -1,4 +1,30 @@
-package cs.superleague.shopping.responses;
+package cs.superleague.user.responses;
+
+import cs.superleague.user.dataclass.Customer;
+
+import java.util.Date;
 
 public class UpdateCustomerDetailsResponse {
+
+    private final String message;
+    private final boolean success;
+    private final Date timestamp;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public UpdateCustomerDetailsResponse(String message, boolean success, Date timestamp) {
+        this.message = message;
+        this.success = success;
+        this.timestamp = timestamp;
+    }
 }
