651b8dcf4144d2a95872ccfec0cae91d329769bf
Seamus-Brennan <u19060468@tuks.co.za>
2021-08-03 17:37:01 +0200

    Created Request and response objects for RetrieveNotification and SendEmailNotification

---
+++ backend/src/main/java/cs/superleague/notification/requests/SendEmailNotificationRequest.java
@@ -0,0 +1,43 @@
+package cs.superleague.notification.requests;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class SendEmailNotificationRequest {
+
+    private final String message;
+    private final UUID userID;
+    private final String subject;
+    private final String type;
+    private final String email;
+
+    public SendEmailNotificationRequest(String message, Map<String, String> properties) {
+        this.message = message;
+        this.type = properties.get("Type");
+        this.subject = properties.get("Subject");
+        this.userID = UUID.fromString(properties.get("UUID"));
+        this.email = properties.get("Email");
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
