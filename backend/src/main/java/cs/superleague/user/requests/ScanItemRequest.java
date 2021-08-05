559d78210e86ee63c285abd736bd116408fbd223
PretoriaDogsRescue <pretoriadogrescue2@gmail.com>
2021-08-05 02:47:22 +0200

    Implemented ScanItemRequest and ScanItemResponse

--- backend/src/main/java/cs/superleague/user/requests/ScanItemRequest.java
+++ backend/src/main/java/cs/superleague/user/requests/ScanItemRequest.java
@@ -1,4 +1,23 @@
 package cs.superleague.user.requests;
 
+import cs.superleague.payment.dataclass.Order;
+
+import java.util.UUID;
+
 public class ScanItemRequest {
+    private Order order;
+    private String barcode;
+
+    public ScanItemRequest(String barcode, Order order) {
+        this.order = order;
+        this.barcode = barcode;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
 }
