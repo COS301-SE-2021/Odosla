55b6b1555bcbdeaa43a9571d95450bf0243749b8
Seamus-Brennan <u19060468@tuks.co.za>
2021-08-05 10:42:20 +0200

    Added SendEmailNotification function

--- backend/src/main/java/cs/superleague/notification/NotificationService.java
+++ backend/src/main/java/cs/superleague/notification/NotificationService.java
@@ -2,12 +2,16 @@ package cs.superleague.notification;
 
 import cs.superleague.notification.requests.CreateNotificationRequest;
 import cs.superleague.notification.requests.RetrieveNotificationRequest;
+import cs.superleague.notification.requests.SendEmailNotificationRequest;
 import cs.superleague.notification.responses.CreateNotificationResponse;
 import cs.superleague.notification.responses.RetrieveNotificationResponse;
+import cs.superleague.notification.responses.SendEmailNotificationResponse;
 
 public interface NotificationService {
 
     CreateNotificationResponse createNotification(CreateNotificationRequest request);
 
     RetrieveNotificationResponse retrieveNotification(RetrieveNotificationRequest request);
+
+    SendEmailNotificationResponse sendEmailNotification(SendEmailNotificationRequest request);
 }
--- backend/src/main/java/cs/superleague/notification/NotificationServiceImpl.java
+++ backend/src/main/java/cs/superleague/notification/NotificationServiceImpl.java
@@ -3,8 +3,10 @@ package cs.superleague.notification;
 import cs.superleague.notification.repos.NotificationRepo;
 import cs.superleague.notification.requests.CreateNotificationRequest;
 import cs.superleague.notification.requests.RetrieveNotificationRequest;
+import cs.superleague.notification.requests.SendEmailNotificationRequest;
 import cs.superleague.notification.responses.CreateNotificationResponse;
 import cs.superleague.notification.responses.RetrieveNotificationResponse;
+import cs.superleague.notification.responses.SendEmailNotificationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,11 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public RetrieveNotificationResponse retrieveNotification(RetrieveNotificationRequest request) {
+        return null;
+    }
+
+    @Override
+    public SendEmailNotificationResponse sendEmailNotification(SendEmailNotificationRequest request) {
         return null;
     }
 
