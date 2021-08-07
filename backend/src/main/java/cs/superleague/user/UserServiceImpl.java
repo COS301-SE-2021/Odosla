2df8570ae6fd548d7766d1cd615a962246722796
mogoatlhe <u14254922@tuks.co.za>
2021-08-07 19:34:44 +0200

    origin develop_user -> develop_user

--- backend/src/main/java/cs/superleague/user/UserServiceImpl.java
+++ backend/src/main/java/cs/superleague/user/UserServiceImpl.java
@@ -16,6 +16,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import cs.superleague.user.exceptions.*;
