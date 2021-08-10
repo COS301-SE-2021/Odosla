8a12295c6dbc631725e46d225d52d4402a231195
kelly <u19085878@tuks.co.za>
2021-08-10 19:06:23 +0200

    Testing

--- backend/src/main/java/cs/superleague/Application.java
+++ backend/src/main/java/cs/superleague/Application.java
@@ -2,10 +2,12 @@ package cs.superleague;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class Application{
 
     public static void main(String[] args) {
