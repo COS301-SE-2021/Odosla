073eedb103ce6fc9dd80e2bd832b31daddac6619
Seamus-Brennan <u19060468@tuks.co.za>
2021-08-10 18:57:43 +0200

    Implemented request and response objects

--- backend/src/main/java/cs/superleague/delivery/requests/AddDeliveryDetailRequest.java
+++ backend/src/main/java/cs/superleague/delivery/requests/AddDeliveryDetailRequest.java
@@ -1,4 +1,54 @@
 package cs.superleague.delivery.requests;
 
+import cs.superleague.delivery.dataclass.DeliveryStatus;
+
+import java.util.Calendar;
+import java.util.UUID;
+
 public class AddDeliveryDetailRequest {
+    private DeliveryStatus status;
+    private String detail;
+    private UUID deliveryID;
+    private Calendar timestamp;
+
+    public AddDeliveryDetailRequest(DeliveryStatus status, String detail, UUID deliveryID, Calendar timestamp) {
+        this.status = status;
+        this.detail = detail;
+        this.deliveryID = deliveryID;
+        this.timestamp = timestamp;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public UUID getDeliveryID() {
+        return deliveryID;
+    }
+
+    public void setDeliveryID(UUID deliveryID) {
+        this.deliveryID = deliveryID;
+    }
+
+    public Calendar getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Calendar timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
 }
--- backend/src/main/java/cs/superleague/delivery/responses/UpdateDeliveryStatusResponse.java
+++ backend/src/main/java/cs/superleague/delivery/responses/UpdateDeliveryStatusResponse.java
@@ -1,4 +1,17 @@
 package cs.superleague.delivery.responses;
 
 public class UpdateDeliveryStatusResponse {
+    private String message;
+
+    public UpdateDeliveryStatusResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
