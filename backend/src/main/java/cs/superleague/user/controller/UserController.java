988f66685b3a997e8f25aba6a377135e72af5743
kelly <u19085878@tuks.co.za>
2021-08-10 19:57:30 +0200

    Testing

---
+++ backend/src/main/java/cs/superleague/user/controller/UserController.java
@@ -0,0 +1,10 @@
+//package cs.superleague.user.controller;
+//
+//import cs.superleague.api.UserApi;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@CrossOrigin
+//@RestController
+//public class UserController implements UserApi {
+//}
