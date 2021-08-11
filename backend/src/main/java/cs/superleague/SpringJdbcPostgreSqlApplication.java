608d6b7937ff0bd0272186831fc88d2bff979600
PretoriaDogsRescue <u19055863@tuks.co.za>
2021-08-11 02:02:24 +0200

    set show-sql to true in application.properties
    added annotations to Shopper.java so that it matches the columns of the table in the db

--- backend/src/main/java/cs/superleague/SpringJdbcPostgreSqlApplication.java
+++ backend/src/main/java/cs/superleague/SpringJdbcPostgreSqlApplication.java
@@ -1,16 +1,14 @@
 package cs.superleague;
 
+import cs.superleague.user.repos.ShopperRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class SpringJdbcPostgreSqlApplication implements CommandLineRunner {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args)
     {
@@ -18,12 +16,6 @@ public class SpringJdbcPostgreSqlApplication implements CommandLineRunner {
     }
     @Override
     public void run(String... args) throws Exception {
-        String sql = "INSERT INTO Shopper (shopperid, storeid, orders_completed) VALUES (" +
-                "'55534567-9CBC-FEF0-1254-56789ABCDEF0', '01234567-9ABC-DEF0-1234-56789ABCDEF0', '0')";
-        int rows = jdbcTemplate.update(sql);
-        if(rows >0)
-        {
-            System.out.println("A new entry has been made");
-        }
+
     }
 }
--- backend/src/main/java/cs/superleague/user/dataclass/Shopper.java
+++ backend/src/main/java/cs/superleague/user/dataclass/Shopper.java
@@ -1,21 +1,55 @@
 package cs.superleague.user.dataclass;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
 
 @Entity
-@Table
+@Table(name = "shopper")
 public class Shopper extends User {
 
     /* Attributes */
     @Id
+    @Column(name = "shopperid")
     private UUID shopperID;
+
+    @Column(name = "storeid")
     private UUID storeID;
+
+    @Column(name = "orders_completed")
     private int ordersCompleted=0;
+
+    private String name;
+    private String surname;
+    private String email;
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+
+    private String password;
+
+    @Column(name="activation_date")
+    private Date activationDate;
+
+    @Column(name="activation_code")
+    private String activationCode;
+
+    @Column(name="reset_code")
+    private String resetCode;
+
+    @Column(name="reset_expiration")
+    private String resetExpiration;
+
+    @Column(name="is_active")
+    private boolean isActive;
+
+    @Column(name="account_type")
+    @Enumerated(EnumType.STRING)
+    private UserType accountType;
+
     //payOption..
 
     public Shopper() {
