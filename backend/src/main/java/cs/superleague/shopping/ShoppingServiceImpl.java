40b43908ece9a0c8a464d8f6b225f72ae1252f28
PretoriaDogsRescue <u19055863@tuks.co.za>
2021-08-17 13:26:02 +0200

    fixed GetStores integration test

--- backend/src/main/java/cs/superleague/shopping/ShoppingServiceImpl.java
+++ backend/src/main/java/cs/superleague/shopping/ShoppingServiceImpl.java
@@ -1181,7 +1181,7 @@ public class ShoppingServiceImpl implements ShoppingService {
 
             }catch (Exception e){}
 
-            if(storeEntity!=null) {
+            if(storeEntity!=null && !storeEntity.isEmpty()) {
 
                 response = new GetStoresResponse(true, "List of Stores successfully returned", storeEntity);
             }
