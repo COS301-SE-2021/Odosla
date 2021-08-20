d1c85703c22cb24e431ea5bb32c87001d8e4f9d9
Adam <adam.isenberg4920@gmail.com>
2021-08-20 07:18:25 +0200

    removed unused function

--- frontend/customer/odosla/lib/services/settings.dart
+++ frontend/customer/odosla/lib/services/settings.dart
@@ -1,3 +1,3 @@
 //final endpoint = "http://localhost:8080";
-//final endpoint = "https://816170cd922b.ngrok.io";
-final endpoint = "https://f1de7630b01d.ngrok.io";
+final endpoint = "https://816170cd922b.ngrok.io";
+//final endpoint = "https://f1de7630b01d.ngrok.io";
