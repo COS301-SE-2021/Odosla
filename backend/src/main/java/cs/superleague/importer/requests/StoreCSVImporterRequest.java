b301a74ee905d8c633eb852ca9bea6aa01e9e29a
PretoriaDogsRescue <u19055863@tuks.co.za>
2021-08-23 18:11:40 +0200

    request and response objects for store importer

---
+++ backend/src/main/java/cs/superleague/importer/requests/StoreCSVImporterRequest.java
@@ -0,0 +1,12 @@
+package cs.superleague.importer.requests;
+
+public class StoreCSVImporterRequest {
+
+    String file;
+
+    public StoreCSVImporterRequest(String file)
+    {
+        this.file=file;
+    }
+
+}
