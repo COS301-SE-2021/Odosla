c71ea779ce26b5fa06ed83907c7bc754619cf4b1
kelly <u19085878@tuks.co.za>
2021-09-06 18:55:18 +0200

    Fixed JIntegration testing with jwt environment variables.

--- backend/src/test/java/cs/superleague/delivery/integration/RemoveDriverFromDeliveryIntegrationTest.java
+++ backend/src/test/java/cs/superleague/delivery/integration/RemoveDriverFromDeliveryIntegrationTest.java
@@ -17,6 +17,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Description;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -42,7 +43,8 @@ public class RemoveDriverFromDeliveryIntegrationTest {
     @Autowired
     DriverRepo driverRepo;
 
-    private final String SECRET = "uQmMa86HgOi6uweJ1JSftIN7TBHFDa3KVJh6kCyoJ9bwnLBqA0YoCAhMMk";
+    @Value("${env.SECRET}")
+    private String SECRET = "stub";
 
     Delivery delivery;
     UUID deliveryID;
