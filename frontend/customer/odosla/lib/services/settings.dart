428f49f0d811de1544d824b93c16849d044a3a76
kelly <u19085878@tuks.co.za>
2021-09-16 16:41:56 +0200

    Updated Settings

--- frontend/customer/odosla/lib/services/settings.dart
+++ frontend/customer/odosla/lib/services/settings.dart
@@ -1,3 +1,3 @@
 //final endpoint = "http://localhost:8080";
 //final endpoint = "https://816170cd922b.ngrok.io";
-final endpoint = "https://f591251010c0.ngrok.io";
+final endpoint = "http://10.0.2.2:8080";
