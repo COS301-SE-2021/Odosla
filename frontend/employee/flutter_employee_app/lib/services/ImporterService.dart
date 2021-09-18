8d5f1f0a991d5747785816aaf1ee8a39efff2aef
kelly <u19085878@tuks.co.za>
2021-09-18 15:04:29 +0200

    Fixed importer service

--- frontend/employee/flutter_employee_app/lib/services/ImporterService.dart
+++ frontend/employee/flutter_employee_app/lib/services/ImporterService.dart
@@ -6,23 +6,21 @@ import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 
 import 'UserService.dart';
+
 class ImporterService {
 
   UserService _userService = GetIt.I.get();
 
   Future<bool?> importItems(BuildContext context, String csv) async {
-    final loginURL = Uri.parse(importerEndPoint + "importer/itemsCSVImporter");
+
+    final URL = Uri.parse(importerEndPoint + "importer/itemsCSVImporter");
 
     Map<String, String> headers = new Map<String, String>();
     String jwt = "";
 
-    // await _userService.getJWTAsString(context).then((value) => {
-    //   jwt=value!
-    // });
-    jwt =
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrZWxseW1vcnJpc29uNzQ1OEBnbWFpbC5jb20iLCJ1c2VyVHlwZSI6IkFETUlOIiwiZXhwIjoxNjMxNTc5MzQxLCJpYXQiOjE2MzE1NDMzNDEsImVtYWlsIjoia2VsbHltb3JyaXNvbjc0NThAZ21haWwuY29tIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9BRE1JTiJdLCJqdGkiOiIzZTYyNTQ3Ny1mMWY1LTRiMzAtOTA0ZC1iMjEyZGU5ODEwOGIifQ.j1nnDAcMNVoWAVuJ983LctwLEIBZI45MupGF-Gh4PsE";
-
-    print(jwt);
+    await _userService.getJWTAsString(context).then((value) => {
+      jwt=value!
+    });
 
     headers =
     {
@@ -38,42 +36,49 @@ class ImporterService {
     };
 
     final response = await http.post(
-        loginURL, headers: headers, body: jsonEncode(data));
-    print(response.body);
+        URL, headers: headers, body: jsonEncode(data)).timeout(
+        Duration(seconds: 15),
+        onTimeout:(){ return(http.Response('TimeOut',408));
+        });
 
     if (response.statusCode == 200) {
+
       Map<String, dynamic> responseData = json.decode(response.body);
-      print("RESPONSE DATA");
+
       if (responseData["success"] == true) {
         return true;
-      } else {
+      }
+      else {
         ScaffoldMessenger.of(context)
             .showSnackBar(
             SnackBar(content: Text("Server couldn't process request")));
       }
-    } else if (response.statusCode == 403) {
+    }
+    else if (response.statusCode == 403) {
       ScaffoldMessenger.of(context)
           .showSnackBar(
           SnackBar(content: Text("You are unauthorized to use this app")));
-    } else if (response.statusCode == 500) {
+    }
+    else if (response.statusCode == 500) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Internal server error")));
+    }
+    else if (response.statusCode==408){
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text("Request timed out")));
-    } else
-      return null;
+    }
+    else return null;
   }
 
   Future<bool?> importStores(BuildContext context, String csv) async {
-    final loginURL = Uri.parse(importerEndPoint + "importer/storesCSVImporter");
+    final URL = Uri.parse(importerEndPoint + "importer/storesCSVImporter");
 
     Map<String, String> headers = new Map<String, String>();
     String jwt = "";
 
-    // await _userService.getJWTAsString(context).then((value) => {
-    //   jwt=value!
-    // });
-    jwt =
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrZWxseW1vcnJpc29uNzQ1OEBnbWFpbC5jb20iLCJ1c2VyVHlwZSI6IkFETUlOIiwiZXhwIjoxNjMxNTc5MzQxLCJpYXQiOjE2MzE1NDMzNDEsImVtYWlsIjoia2VsbHltb3JyaXNvbjc0NThAZ21haWwuY29tIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9BRE1JTiJdLCJqdGkiOiIzZTYyNTQ3Ny1mMWY1LTRiMzAtOTA0ZC1iMjEyZGU5ODEwOGIifQ.j1nnDAcMNVoWAVuJ983LctwLEIBZI45MupGF-Gh4PsE";
-
+    await _userService.getJWTAsString(context).then((value) => {
+      jwt=value!
+    });
     print(jwt);
 
     headers =
@@ -90,27 +95,37 @@ class ImporterService {
     };
 
     final response = await http.post(
-        loginURL, headers: headers, body: jsonEncode(data));
-    print(response.body);
+        URL, headers: headers, body: jsonEncode(data)).timeout(
+        Duration(seconds: 15),
+        onTimeout:(){ return(http.Response('TimeOut',408));
+        });;
 
     if (response.statusCode == 200) {
+
       Map<String, dynamic> responseData = json.decode(response.body);
-      print("RESPONSE DATA");
+
       if (responseData["success"] == true) {
         return true;
-      } else {
+      }
+      else {
         ScaffoldMessenger.of(context)
             .showSnackBar(
             SnackBar(content: Text("Server couldn't process request")));
       }
-    } else if (response.statusCode == 403) {
+    }
+    else if (response.statusCode == 403) {
       ScaffoldMessenger.of(context)
           .showSnackBar(
           SnackBar(content: Text("You are unauthorized to use this app")));
-    } else if (response.statusCode == 500) {
+    }
+    else if (response.statusCode == 500) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Internal server error")));
+    }
+    else if (response.statusCode==408){
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text("Request timed out")));
-    } else
-      return null;
+    }
+    else return null;
   }
 }
