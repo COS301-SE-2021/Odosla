3c021f49b89216ba7b3f8d688031dec98c51d872
kelly <u19085878@tuks.co.za>
2021-10-02 22:35:31 +0200

    frontEnd_employee

--- frontend/employee/flutter_employee_app/lib/pages/shopper/barcode_scanner_screen.dart
+++ frontend/employee/flutter_employee_app/lib/pages/shopper/barcode_scanner_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
@@ -7,15 +6,23 @@ import 'package:flutter_employee_app/pages/shopper/alternative_product_page.dart
 
 class BarcodeScanPage extends StatelessWidget {
   String barcode = 'Unknown barcode';
-  bool _correctBarcode=false;
+  bool _correctBarcode = false;
   final String barcodeExpected;
   final String productImageURL;
   final String productName;
   final String brand;
   final Store store;
-  BarcodeScanPage(BuildContext context, {Key? key, required this.barcodeExpected, required this.productImageURL, required this.productName, required this.brand, required this.store}) : super(key: key);
 
-  Widget _popUpItemNotAvailable(BuildContext context){
+  BarcodeScanPage(BuildContext context,
+      {Key? key,
+      required this.barcodeExpected,
+      required this.productImageURL,
+      required this.productName,
+      required this.brand,
+      required this.store})
+      : super(key: key);
+
+  Widget _popUpItemNotAvailable(BuildContext context) {
     return new AlertDialog(
       title: const Text('OUT OF STOCK'),
       content: new Column(
@@ -23,8 +30,7 @@ class BarcodeScanPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text("Would you like to offer an alternative? ",
-              textAlign: TextAlign.center
-          ),
+              textAlign: TextAlign.center),
         ],
       ),
       actions: <Widget>[
@@ -32,9 +38,7 @@ class BarcodeScanPage extends StatelessWidget {
           onPressed: () async {
             showDialog(
               context: context,
-              builder: (BuildContext context) =>
-                  _offerAlternative(
-                      context),
+              builder: (BuildContext context) => _offerAlternative(context),
             );
             Navigator.pop(context, false);
           },
@@ -54,24 +58,27 @@ class BarcodeScanPage extends StatelessWidget {
     );
   }
 
-  Widget _offerAlternative(BuildContext context){
+  Widget _offerAlternative(BuildContext context) {
     return new AlertDialog(
       title: const Text('Alternative'),
       content: new Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text("Would you prefer to select an item from the page or scan its barcode? ",
-              textAlign: TextAlign.center
-          ),
+          Text(
+              "Would you prefer to select an item from the page or scan its barcode? ",
+              textAlign: TextAlign.center),
         ],
       ),
       actions: <Widget>[
         new FlatButton(
           onPressed: () async {
             Navigator.of(context).push(MaterialPageRoute(
-                builder: (BuildContext context) => AlternativeItemsPage(store.id, store.name,
-                    {"lat": double.parse(store.storeLocationLatitude),"long":double.parse(store.storeLocationLongitude)})));
+                builder: (BuildContext context) =>
+                    AlternativeItemsPage(store.id, store.name, {
+                      "lat": double.parse(store.storeLocationLatitude),
+                      "long": double.parse(store.storeLocationLongitude)
+                    })));
           },
           child: const Text('Items page'),
         ),
@@ -88,119 +95,125 @@ class BarcodeScanPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: Text("Barcode scanner"),
-    ),
-    body: Container(
-      height: MediaQuery.of(context).size.height,
-      alignment: Alignment.center,
-      child: ListView(
-        children: <Widget>[
-          Center(
-            child: Container(
-              height:MediaQuery.of(context).size.height*0.22,
-              child:Image(
-                fit: BoxFit.fill,
-                image: AssetImage("assets/"+productImageURL),
-              ),
-            ),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height*0.07,
-            child: Column(
-              children: [
-                Text(
-                  productName,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          title: Text("Barcode scanner"),
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          alignment: Alignment.center,
+          child: ListView(
+            children: <Widget>[
+              Center(
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.22,
+                  child: Image(
+                    fit: BoxFit.fill,
+                    image: AssetImage("assets/" + productImageURL),
                   ),
                 ),
-                Text(
-                  brand,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.07,
+                child: Column(
+                  children: [
+                    Text(
+                      productName,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      brand,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.09,
+                color: Colors.deepOrangeAccent,
+                alignment: Alignment.center,
+                child: Text(
+                  _correctBarcode == false
+                      ? "Barcode expected: " + barcodeExpected
+                      : "Correct barcode scanned",
                   style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Center(
+                child: ButtonWidget(
+                  text: 'Start Barcode scan',
+                  onClicked: () async {
+                    await scanBarcode(context);
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.0),
+                    border: Border.all(color: Colors.deepOrangeAccent),
+                  ),
+                  alignment: Alignment.topCenter,
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(fontFamily: 'OpenSans', fontSize: 25),
+                    onChanged: (value) => {
+                      barcode = value,
+                      if (value == barcodeExpected)
+                        {Navigator.pop(context, true)}
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 14.0),
+                      hintText: 'Product ID',
+                    ),
                   ),
                 ),
-              ],
-            ),
-          ),
-          SizedBox(height: 10,),
-          Container(
-            height: MediaQuery.of(context).size.height*0.09,
-            color: Colors.deepOrangeAccent,
-            alignment: Alignment.center,
-            child: Text(
-              _correctBarcode==false?"Barcode expected: "+barcodeExpected:"Correct barcode scanned",
-              style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white
               ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          SizedBox(height: 40,),
-          Center(
-            child: ButtonWidget(
-              text: 'Start Barcode scan',
-              onClicked: () async {
-                await scanBarcode(context);},
-            ),
-          ),
-          SizedBox(height: 10,),
-          Center(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.0),
-                border:Border.all(
-                    color: Colors.deepOrangeAccent
-                ),
-              ),
-              alignment: Alignment.topCenter,
-              width: MediaQuery.of(context).size.width*0.7,
-              child: TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(
-                    fontFamily: 'OpenSans',
-                    fontSize: 25
-                ),
-                onChanged: (value) =>
-                {
-                  barcode=value,
-                  if(value==barcodeExpected){
-                    Navigator.pop(context, true)
-                  }
+              GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) =>
+                        _popUpItemNotAvailable(context),
+                  );
                 },
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(top: 14.0),
-                  hintText: 'Product ID',
-                ),
-              ),
-            ),
+                child: Container(
+                    height: 30,
+                    alignment: Alignment.bottomCenter,
+                    child: Text(
+                      "No items available?",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeight.w600),
+                    )),
+              )
+            ],
           ),
-          GestureDetector(
-            onTap: (){
-              showDialog(
-                context: context,
-                builder: (BuildContext context) =>
-                    _popUpItemNotAvailable(
-                        context),
-              );
-            },
-            child: Container(
-                height: 30,
-                alignment: Alignment.bottomCenter,
-                child: Text("No items available?", textAlign: TextAlign.center, style: TextStyle(fontStyle: FontStyle.italic,  decoration: TextDecoration.underline, fontWeight: FontWeight.w600),)
-            ),
-          )
-        ],
-      ),
-    ),
-  );
+        ),
+      );
 
   Future scanBarcode(BuildContext context) async {
     try {
@@ -210,22 +223,19 @@ class BarcodeScanPage extends StatelessWidget {
         true,
         ScanMode.BARCODE,
       );
-        if(barcode==barcodeExpected){
-          this._correctBarcode=true;
-          Navigator.pop(context, true);
-        }
-        else{
-          this._correctBarcode=false;
-        }
+      if (barcode == barcodeExpected) {
+        this._correctBarcode = true;
+        Navigator.pop(context, true);
+      } else {
+        this._correctBarcode = false;
       }
-      on Exception {
+    } on Exception {
       barcode = 'Failed to get platform version.';
     }
   }
 }
 
-class ButtonWidget extends StatelessWidget{
-
+class ButtonWidget extends StatelessWidget {
   final String text;
   final VoidCallback onClicked;
 
@@ -236,13 +246,13 @@ class ButtonWidget extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) => RaisedButton(
-    child: Text(
-      text,
-      style: TextStyle(fontSize: 24),
-    ),
-    shape: StadiumBorder(),
-    color: Theme.of(context).primaryColor,
-    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 3),
-    onPressed: onClicked,
-  );
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 24),
+        ),
+        shape: StadiumBorder(),
+        color: Theme.of(context).primaryColor,
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 3),
+        onPressed: onClicked,
+      );
 }
